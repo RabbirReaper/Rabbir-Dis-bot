2e69801194ed7ba1a90e6a98f5e8b4e7e3ed8db2
Rabbir_Reaper <abc0979999412@gmail.com>
2023-12-15 20:47:02 +0800

    把!cpp 改成```cpp 裡面的cpp 做判定 完善了整個系統

--- src/events/keyWordReaction/index.js
+++ src/events/keyWordReaction/index.js
@@ -10,13 +10,12 @@ export const event = {
 export const action = async(message) =>{
     if (message.author.bot) return;
     message.content = message.content.replace("<@1007521034515271742>","")
-    const check  = '!cpp'
+    const check  = 'cpp'
     message.content = message.content.trim();
     // console.log(message.content)
 
-    let k = message.content.lastIndexOf(check)
-    if(k === 0){
-        const code = message.content.slice(message.content.indexOf("```")+3, message.content.indexOf("``````")) // 從訊息文本中提取程式碼
+    if(message.content.substr(3,3) === check){
+        const code = message.content.slice(message.content.indexOf("cpp")+3, message.content.indexOf("``````")) // 從訊息文本中提取程式碼
         const userId = message.author.id; 
         let dir = "./userCodes"; // 存儲程式碼文件的文件夾
     
@@ -25,19 +24,17 @@ export const action = async(message) =>{
         }
     
         fs.writeFileSync(`${dir}/${userId}.cpp`, code) // 將提取出的程式碼保存到文件夾中
-        exec(`g++ ${dir}\/${userId}.cpp -o ${dir}\/${userId}.exe`, (err, stdout, stdin) => { // 使用 g++ 命令編譯 C++ 程式
+        exec(`g++ -Wall -Wextra -O2 -std=c++14 ${dir}\/${userId}.cpp -o ${dir}\/${userId}.exe`, (err, stdout, stdin) => { // 使用 g++ 命令編譯 C++ 程式
+            //  "cd $dir & g++ -Wall -Wextra -O2 -std=c++14 \"$fileName\" -o \"$fileNameWithoutExt\" & $dir\"$fileNameWithoutExt\""
             if (err != null) { // 如果編譯出錯
                 message.reply("```\n" + err.toString().slice(0, 1500) + "```") // 將錯誤消息傳回到 Discord 服務器
                 return;
             }
-            // message.reply("Your code compiled successfully run ```!input``` to execute your program") // 如果編譯成功，則提示用戶可以執行程式碼
         })
 
         dir = `userCodes\\${userId}.exe`;
 
-        const input = message.content.slice(message.content.indexOf('``````')+6, message.content.lastIndexOf('``````')) // 從Discord訊息獲取輸入的文本
-        let ans = ''
-        ans = message.content.slice(message.content.lastIndexOf('``````')+6, message.content.lastIndexOf('```')).trim()
+        const input = message.content.slice(message.content.indexOf('``````')+6, message.content.lastIndexOf('```')) // 從Discord訊息獲取輸入的文本
         const childProcess = spawn(dir, [], { stdio: ['pipe', 'pipe', 'pipe'] });
         childProcess.stdin.write(input);
         childProcess.stdin.end();
@@ -60,9 +57,7 @@ export const action = async(message) =>{
             if (code !== 0 && isTimeout) {
                 message.reply("TLE"); // 回覆 Discord 用戶 TLE
             }else if(code === 0) {
-                if(ans.replace(/\s+/g, ' ').trim() === outputData.replace(/\s+/g, ' ').trim()) message.reply("AC");
-                else message.reply("WA");
-                console.log(`${outputData.replace(/\s+/g, ' ').trim()} : ${ans.replace(/\s+/g, ' ').trim()}`)
+                message.reply(`\`\`\`${outputData.replace(/\s+/g, ' ').trim()}\`\`\``);
             }else{
                 message.reply('SE');
             }
