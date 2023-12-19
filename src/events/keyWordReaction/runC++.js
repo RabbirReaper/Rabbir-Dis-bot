74c82e476d11904349ed1c6c43be1c09785bb754
Rabbir_Reaper <abc0979999412@gmail.com>
2023-12-19 23:28:15 +0800

    增加顯示執行時間的功能

--- src/events/keyWordReaction/runC++.js
+++ src/events/keyWordReaction/runC++.js
@@ -1,43 +1,43 @@
-import { spawn} from 'child_process';
+import {spawn} from 'child_process';
 
 export const run = async (input, userId, message) => {
     const dir = `userCodes\\${userId}.exe`;
 
     try {
-        const process = spawn(dir);
+        const startTime = process.hrtime(); // 程式啟動時間
+        const childProcess = spawn(dir);
         let isTimeout = false
         let output = "";
         const timeout = setTimeout(() => {
             isTimeout = true;
-            process.kill(); // 終止子進程
+            childProcess.kill(); // 終止子進程
         }, 1000);
 
-        process.stdout.on('data', (data) => {
+        childProcess.stdout.on('data', (data) => {
             output += data.toString();
         });
 
-        process.stderr.on('data', (data) => {
+        childProcess.stderr.on('data', (data) => {
             console.error("Error:", data);
             message.reply("程式執行錯誤！");
         });
 
-        process.on('close', () => {
+        childProcess.on('close', () => {
             if(isTimeout){
                 message.reply("```\nTLE```");
             }else{
+                const endTime = process.hrtime(startTime); // 計算執行時間差值
                 output.trim()
                 if(output.length > 1000){
                     output = output.slice(0, 1000)
                     output += "\n...(輸出被截斷)..."
-                    message.reply("```\n"+output+"```")
-                    return;
                 }
-                message.reply("```\n"+output+"```")
+                message.reply("```\n"+output+"```"+"```\n執行時間 : " + (endTime[1]/1e6).toFixed(0) +"豪秒```")
             }
         });
 
-        process.stdin.write(input);
-        process.stdin.end();
+        childProcess.stdin.write(input);
+        childProcess.stdin.end();
     } catch (error) {
         console.error("Error:", error);
         message.reply("```\n程式執行錯誤 !```");
